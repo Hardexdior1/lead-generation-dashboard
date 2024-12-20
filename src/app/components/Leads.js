2b379ae07432ebf73806bef60f251543fc91aef4
adeniran <adeniranquwam001@gmail.com>
2024-12-20 15:44:58 +0100

    removed-unused-variable

--- src/app/components/Leads.js
+++ src/app/components/Leads.js
@@ -181,24 +181,7 @@ const page = () => {
     },
   ];
 
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 1024 },
-      items: 2,
-    },
-    desktop: {
-      breakpoint: { max: 1024, min: 768 },
-      items: 2,
-    },
-    tablet: {
-      breakpoint: { max: 768, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+  
   const responsive2 = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 1024 },
